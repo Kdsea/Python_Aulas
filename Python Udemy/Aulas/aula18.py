546ab063de91a6edc68ba777811d422e6fa439ac
Kaua de Souza <k2183109@gmail.com>
2025-04-06 15:41:50 -0300

    adicionado aula 18 e conteudo

---
+++ Python Udemy/Aulas/aula18.py
@@ -0,0 +1,19 @@
+'''
+operadores de comparação (relacionais)
+OP      Significado         Exemplo
+>       maior que           2 > 1
+>=      maior ou igual a    2 >= 2
+<       menor que           1 < 2
+<=      menor ou igual a    2 <= 2
+==      igual a            'a' == 'b'
+!=      diferente de        'a' != 'b'
+
+'''
+
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual = 2 <= 2
+igual = 'a' == 'b'
+diferente = 'a' != 'b'
+print(diferente)
