524325850ee8cb998634cba09865e64443ae8073
Kaua de Souza <k2183109@gmail.com>
2025-04-06 15:22:03 -0300

    Conteudo adicionado a aula 17

--- Python Udemy/Aulas/aula17.py
+++ Python Udemy/Aulas/aula17.py
@@ -1,1 +1,28 @@
-23
+# if / elif       / else
+# se / se não se / se não
+condicao = False
+condicao1= False
+condicao2= False
+condicao3= False
+condicao4= False
+
+#as condições serao testadas na ordem em que foram escritas
+#se a primeira for verdadeira, as demais nao serao testadas
+#se a primeira for falsa, a segunda sera testada e assim por diante
+#se nenhuma for verdadeira, o else sera executado
+
+if condicao1:
+    print('Este é o codigo do if')
+elif condicao2:
+    print('codigo para condicao 2')
+elif condicao3:
+    print('codigo para condicao 3')
+elif condicao4:
+    print('codigo para condicao 4')
+else: 
+    print('Nenhuma condicao foi atendida')
+
+if 10 == 10:
+    print('Outro If')
+
+print('Fora do if')
