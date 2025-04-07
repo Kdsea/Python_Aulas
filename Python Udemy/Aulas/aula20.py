8d3effa6814630fb3b29fdac3ed51d51a6ecfb8f
Kaua de Souza <k2183109@gmail.com>
2025-04-07 19:17:31 -0300

    adicionado novas aulas

---
+++ Python Udemy/Aulas/aula20.py
@@ -0,0 +1,18 @@
+#Operadores lógicos and (e), or (ou), not (não)
+#and - todas as condições precisam ser verdadeiras
+#se qualquer valor for considerado falso, a expressão inteira será avaliada naquele valor
+#São considerados falsy 
+#0 0.0 ' ' false
+#tambem existe o tipo None que é usado para representar um não valor
+
+#entrada = input('Você quer comprar pão? [s/n] ')
+#senha_digitada = input('Digite a senha: ')
+#senha_permitida = '123456'
+
+#if entrada == 's' and senha_digitada == senha_permitida:
+#    print('Entrar')
+#else:
+#    print('Sair')
+
+#Avaliação de curto-circuito
+print(True and False and True)
